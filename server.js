a3b528f35dd58c483155031eef5e618ea40b885a
unknown <ratlarva@gmail.com>
2017-09-11 21:16:24 -0600

    fix

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.post("/api/new", function(req, res) {
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(process.env.PORT || PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
